18be52f994b97cd84816df00c66f46344655ffc3
Emanuel Petre <petreemanuel@gmail.com>
2012-01-26 14:58:56 -0500

    Init

---
+++ firefox/Quick definition (Beta)/content/defire.js
@@ -0,0 +1,20 @@
+    var myExtension = {  
+        init: function() {  
+            if(gBrowser) gBrowser.addEventListener("DOMContentLoaded", this.onPageLoad, false);  
+        },  
+        onPageLoad: function(aEvent) {  
+            var doc = aEvent.originalTarget; 
+            var win = doc.defaultView; 
+ 
+            doc.onkeydown = function(e) {
+			
+			 if (e.ctrlKey && e.shiftKey && e.alttKey) {
+				win.open("https://www.google.com/search?client=ubuntu&channel=fs&q=define " + content.getSelection(), '_blank');
+			 }
+		}
+        }  
+    }  
+    window.addEventListener("load", function load(event){  
+        window.removeEventListener("load", load, false);
+        myExtension.init();    
+    },false);
